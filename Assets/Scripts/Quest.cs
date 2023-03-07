d6ece8425267e10aa9869c316164422d824b07f8
Sayan Bose <sayan.bose.lmg.2000@gmail.com>
2023-03-07 12:47:43 +0530

    Implemented a timer functionality which limits the time the player can spend while answering or viewing the answer

--- Assets/Scripts/Quest.cs
+++ Assets/Scripts/Quest.cs
@@ -1,24 +1,126 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Quest : MonoBehaviour
 {
-    [SerializeField] private QuestionSO question;
+    [Header("Question")] [SerializeField] private QuestionSO question;
     [SerializeField] private TextMeshProUGUI questionText;
-    [SerializeField] private GameObject[] options;
-    [SerializeField] private int correctOption;
+    [Header("Answer")] [SerializeField] private GameObject[] options;
+    [SerializeField] private int correctOptionIndex;
+    [Header("Sprite")] [SerializeField] private Sprite defaultOptionSprite;
+    [SerializeField] private Sprite correctOptionSprite;
+    [Header("Timer")] [SerializeField] private Timer timer;
 
-    void Start()
+    private bool _hasAnswered;
+
+
+    private void Start()
+    {
+        InitializeQuestion();
+        _hasAnswered = false;
+    }
+
+    private void Update()
+    {
+        timer.UpdateTimer();
+        if (timer.canLoadNextQuestion)
+        {
+            _hasAnswered = false;
+            GetNextQuestion();
+            timer.canLoadNextQuestion = false;
+        }
+        else if (!_hasAnswered && !timer.isAnswering)
+        {
+            OnSelectOption(-1);
+        }
+    }
+
+    private void InitializeQuestion()
     {
         questionText.text = question.GetQuestion();
+        questionText.color = Color.cyan;
         for (var i = 0; i < options.Length; i++)
         {
             options[i].GetComponentInChildren<TextMeshProUGUI>().text = question.GetOption(i);
         }
 
-        correctOption = question.GetCorrectOptionIndex();
+        correctOptionIndex = question.GetCorrectOptionIndex();
+    }
+
+    public void OnSelectOption(int selectedOptionIndex)
+    {
+        if (selectedOptionIndex == correctOptionIndex)
+        {
+            DisplayResult(0);
+        }
+        else if (selectedOptionIndex != -1)
+        {
+            DisplayResult(1);
+        }
+        else
+        {
+            DisplayResult(2);
+            return;
+        }
+
+        _hasAnswered = true;
+        timer.CancelTimer();
+    }
+
+    private void DisplayResult(int result)
+    {
+        if (result == 0)
+        {
+            // correct answer
+            questionText.text = "Correct!";
+            questionText.color = Color.green;
+        }
+        else if (result == 1)
+        {
+            // incorrect answer
+            questionText.text = "Incorrect!\n";
+            questionText.text +=
+                "Answer is: " + options[correctOptionIndex].GetComponentInChildren<TextMeshProUGUI>().text;
+            questionText.color = Color.red;
+        }
+        else
+        {
+            // no answer
+            questionText.text = "Timer ran out!\n";
+            questionText.text +=
+                "Answer is: " + options[correctOptionIndex].GetComponentInChildren<TextMeshProUGUI>().text;
+            questionText.color = Color.magenta;
+        }
+
+        Image correctButtonImage = options[correctOptionIndex].GetComponent<Image>();
+        correctButtonImage.sprite = correctOptionSprite;
+        SetButtonState(false);
+    }
+
+    private void SetButtonState(bool canEnable)
+    {
+        foreach (var buttonObject in options)
+        {
+            buttonObject.GetComponent<Button>().interactable = canEnable;
+        }
+    }
+
+    private void GetNextQuestion()
+    {
+        InitializeQuestion();
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+    }
+
+    private void SetDefaultButtonSprites()
+    {
+        foreach (var buttonObjects in options)
+        {
+            buttonObjects.GetComponent<Button>().image.sprite = defaultOptionSprite;
+        }
     }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float _timeLeft;
+    public bool isAnswering;
+    public bool canLoadNextQuestion;
+
+    [SerializeField] private float displayQuestionTime = 7f, displayAnswerTime = 4f;
+    [SerializeField] private Image timerImage;
+
+    private void Start()
+    {
+        timerImage = GetComponent<Image>();
+        _timeLeft = displayQuestionTime;
+        isAnswering = true;
+        canLoadNextQuestion = false;
+    }
+
+    public void UpdateTimer()
+    {
+        _timeLeft -= Time.deltaTime;
+        timerImage.fillAmount = (_timeLeft / (isAnswering ? displayQuestionTime : displayAnswerTime));
+
+        if (_timeLeft > 0) return;
+
+        // Timer has run out
+        // Toggle state from answering to viewing or vice versa
+        if (isAnswering)
+        {
+            _timeLeft = displayAnswerTime;
+            isAnswering = false;
+        }
+        else
+        {
+            _timeLeft = displayQuestionTime;
+            isAnswering = true;
+            canLoadNextQuestion = true;
+        }
+
+        // Refill the timer
+        timerImage.fillAmount = 1f;
+    }
+
+    public void CancelTimer()
+    {
+        _timeLeft = 0;
+    }
+}
